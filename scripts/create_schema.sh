019fe4ccd73501d0dd9f638569506819f75d7466
wuerike <wuerike.henri@gmail.com>
2023-10-21 00:32:21 -0300

    Update scripts

--- scripts/create_schema.sh
+++
@@ -1,8 +0,0 @@
-curl --request POST \
---url http://localhost:8085/subjects/events-value/versions \
---header 'Content-Type: application/json' \
---header 'User-Agent: insomnia/2023.5.8' \
---data '{ 
-    "schema": "{\"type\":\"object\",\"properties\":{\"id\":{\"type\":\"string\"},\"type\":{\"type\":\"string\"}},\"required\":[\"id\",\"type\"]}", 
-    "schemaType": "JSON"
-}'
